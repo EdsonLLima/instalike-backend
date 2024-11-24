833bf7dc8c36f4a430384526500aa3e7a9f5237c
Edson L Lima <hd.consultor@hotmail.com>
2024-11-24 11:16:17 -0300

    add resource for salve windows for Multer

--- src/routers/postsRouters.js
+++ src/routers/postsRouters.js
@@ -2,7 +2,16 @@ import express from "express";
 import multer from "multer";
 import { createNewPost, postsList, uploadImage } from "../controllers/postsController.js";
 
-const upload = multer({ dest: "./uploads" })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+
+const upload = multer({ dest: "./uploads", storage })
 
 const routes = (app) => {
   app.use(express.json())
