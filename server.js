c639255dfc516bd50c5edb781ad15f16357664a5
Edson L Lima <hd.consultor@hotmail.com>
2024-11-24 09:41:56 -0300

    create router post

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import routes from "./src/routers/postsRouter.js";
+import routes from "./src/routers/postsRouters.js";
 
 const app = express();
 routes(app)
--- src/routers/postsRouters.js
+++ src/routers/postsRouters.js
@@ -4,6 +4,7 @@ import { postsList } from "../controllers/postsController.js";
 const routes = (app) => {
   app.use(express.json())
   app.get("/posts", postsList);
+  app.post("/posts",)
 }
 
 export default routes;
