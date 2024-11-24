880781d8a6b4c88e2057b7444292ad9eb7409c64
Edson L Lima <hd.consultor@hotmail.com>
2024-11-24 11:06:14 -0300

    create route upload

--- src/routers/postsRouters.js
+++ src/routers/postsRouters.js
@@ -1,10 +1,14 @@
 import express from "express";
+import multer from "multer";
 import { createNewPost, postsList } from "../controllers/postsController.js";
+
+const upload = multer({ dest: "./uploads" })
 
 const routes = (app) => {
   app.use(express.json())
   app.get("/posts", postsList);
   app.post("/posts", createNewPost)
+  app.post("/upload", upload.single("image"), uploadImage)
 }
 
 export default routes;
