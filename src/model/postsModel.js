63512d2aa1a7ffcff8957f6841541d584bdc24a2
Edson L Lima <hd.consultor@hotmail.com>
2024-11-26 00:40:55 -0300

    function updatePost

--- src/model/postsModel.js
+++ src/model/postsModel.js
@@ -1,3 +1,4 @@
+import { ObjectId } from "mongodb";
 import ConnectBank from "../config/dbconfig.js";
 const connection = await ConnectBank(process.env.STRING_CONNECTION);
 
@@ -12,4 +13,10 @@ export async function createPost(newPost) {
   const db = connection.db("instabyte-imersao")
   const collection = db.collection("posts")
   return collection.insertOne(newPost)
+}
+export async function updatePost(id, newPost) {
+  const db = connection.db("instabyte-imersao")
+  const collection = db.collection("posts")
+  const objID = ObjectId.createFromHexString(id)
+  return collection.updateOne({ _id: new ObjectId(objID) }, { $set: newPost })
 }
